a0258d8ef53c4fac88ae89d6263dc7bfc5dcd7e9
Paul Spieker <p.spieker@duenos.de>
2011-08-18 21:51:08 +0200

    add a variable @snip_id to all cell actions to make the id available in the templates (refs #2)

--- app/cells/content_cell.rb
+++ app/cells/content_cell.rb
@@ -3,27 +3,32 @@ class ContentCell < Cell::Rails
   
   def text_widget(args)
     @options = args[:options]
+    @snip_id = args[:snip_id]
     render
   end
   
   def rich_text_widget(args)
     @options = args[:options]
+    @snip_id = args[:snip_id]
     render
   end
   
   def twitter_stream(args)
     @options = args[:options]
+    @snip_id = args[:snip_id]
     render
   end
   
   def articles_list(args)
     @articles = Article.published.not_reserved.all
+    @snip_id = args[:snip_id]
     render
   end
   
   
   def download_widget(args)
     @options = args[:options]
+    @snip_id = args[:snip_id]
     render
   end
   
