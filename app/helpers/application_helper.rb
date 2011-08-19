1b31b3cc65198a36b4bd7ee023b4ee6d548b9159
Paul Spieker <p.spieker@duenos.de>
2011-08-19 07:45:37 +0200

    adding frontend links to the snippets for editing and deleting them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
       # The extra_class clan be set in the backend for some snippets and can be used to give a specific file to this
       # special snipit.
       out << "<div class=\"snippet #{extra_class}\" id=\"snippet-#{snip.id}\">"
+      out << "<div class=\"controlls\" id=\"snippet-controlls-#{snip.id}\">"
+      out << link_to_function("<span>#{t('admin.general.delete')}</span>".hs, "if(confirm('#{t('admin.pages.are_you_sure_to_delete', :name => snip.title)}')) removeSnip(#{snip.id}, '#{admin_page_snippet_path(snip.page.id, snip.id, :format => :json)}')", :class => "delete")
+      out << link_to("<span>#{t('admin.general.edit')}</span>".hs, edit_admin_page_snippet_path(snip.page.id, snip.id), :class => "edit")
+      out << "</div>"
       out << render_cell(snip.cell_controller, snip.cell_action, :page => page, :options => snip.options, :snip_id => snip.id)
       out << "</div>"
     end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function removeSnip(snip_id, url) {
+  jQuery.ajax({
+    url: url,
+    type: 'delete',
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    cache: false,
+    success: function(data, textStatus, jqXHR) {
+      $('#snippet-'+snip_id).remove();
+    },
+    error: function(data, textStatus, jqXHR) {
+      result = jQuery.parseJSON(data.responseText);
+      alert(result.message);
+    }
+  });
+}
