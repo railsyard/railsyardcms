f622810870e699921b91daf9c03098ef6b3c3adf
Silvio Relli <silvio@relli.org>
2011-08-19 10:45:46 +0200

    Controls on fronted only if user is admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,18 @@ module ApplicationHelper
     page.snippets.for_area(area).map do |snip|
       # If there is a value 'extra_class' is defined in the options, these will be assigned to the *div*
       # surrounding the snippet
-      extra_class = (snip.options.is_a?(Hash) && snip.options.has_key?(:extra_class)) ? snip.options[:extra_class] : ""
+      div_class = (snip.options.is_a?(Hash) && snip.options.has_key?(:div_class)) ? snip.options[:div_class] : ""
       
       # The snippet has a class *snippet*. You can give a default style to all snippets by using this class.
       # The extra_class clan be set in the backend for some snippets and can be used to give a specific file to this
       # special snipit.
-      out << "<div class=\"snippet #{extra_class}\" id=\"snippet-#{snip.id}\">"
-      out << "<div class=\"controlls\" id=\"snippet-controlls-#{snip.id}\">"
-      out << link_to_function("<span>#{t('admin.general.delete')}</span>".hs, "if(confirm('#{t('admin.pages.are_you_sure_to_delete', :name => snip.title)}')) removeSnip(#{snip.id}, '#{admin_page_snippet_path(snip.page.id, snip.id, :format => :json)}')", :class => "delete")
-      out << link_to("<span>#{t('admin.general.edit')}</span>".hs, edit_admin_page_snippet_path(snip.page.id, snip.id), :class => "edit")
-      out << "</div>"
+      out << "<div class=\"snippet #{div_class}\" id=\"snippet-#{snip.id}\">"
+      if current_user && current_user.is_admin?
+        out << "<div class=\"controlls\" id=\"snippet-controlls-#{snip.id}\">"
+        out << link_to_function("<span>#{t('admin.general.delete')}</span>".hs, "if(confirm('#{t('admin.pages.are_you_sure_to_delete', :name => snip.title)}')) removeSnip(#{snip.id}, '#{admin_page_snippet_path(snip.page.id, snip.id, :format => :json)}')", :class => "delete")
+        out << link_to("<span>#{t('admin.general.edit')}</span>".hs, edit_admin_page_snippet_path(snip.page.id, snip.id), :class => "edit")
+        out << "</div>"
+      end
       out << render_cell(snip.cell_controller, snip.cell_action, :page => page, :options => snip.options, :snip_id => snip.id)
       out << "</div>"
     end
