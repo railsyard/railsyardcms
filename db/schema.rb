c5d19d0635ccbb272f1433d05788facd56f87197
Silvio Relli <silvio@relli.org>
2011-08-22 10:13:30 +0200

    fixed german language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110819084751) do
+ActiveRecord::Schema.define(:version => 20110822080752) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
