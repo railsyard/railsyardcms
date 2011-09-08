d78525a1b4c337e60712f4f3081600c8f710adca
Paul Spieker <p.spieker@duenos.de>
2011-09-08 19:43:00 +0200

    test with sqlite and mysql

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -3,7 +3,4 @@ sh -e /etc/init.d/xvfb start
 
 # Create a database.yml for the right database
 echo "Setting up database.yml for $DB"
-cp config/database-example.yml config/database.yml
-if [ "$DB" = "postgres" ]; then
-  sed -i 's/*mysql/*postgres/' config/database.yml
-fi
+cp "config/database-$DB-example.yml" config/database.yml
