a25b3708d9374497979d6eb316a22fd481ea9141
Silvio Relli <silvio@relli.org>
2011-09-09 10:36:05 +0200

    new version ckeditor gem model

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,4 @@
-require 'mime/types'
-
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
-  
-  attr_accessible :data, :assetable_type, :assetable_id, :assetable
+  include Ckeditor::Backend::Paperclip
 end
