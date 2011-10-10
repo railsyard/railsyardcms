6493e176833b9a508a2233c7e9546c504e4c5613
Silvio Relli <silvio@relli.org>
2011-10-10 18:00:14 +0200

    Improved snippets cells structure

--- app/cells/content_cell.rb
+++ app/cells/content_cell.rb
@@ -1,34 +1,33 @@
 class ContentCell < Cell::Rails
+  
+  extend ActiveSupport::Memoizable
   include Yard
   helper_method :yard_home, :get_lang, :get_yard_url, :get_article_url # coming from lib/yard
   
   def text_widget(args)
-    @options = args[:options] ||= {}
-    @snip_id = args[:snip_id]
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     render
   end
   
   def rich_text_widget(args)
-    @options = args[:options] ||= {}
-    @snip_id = args[:snip_id]
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     render
   end
   
   def twitter_stream(args)
-    @options = args[:options] ||= {}
-    @snip_id = args[:snip_id]
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     render
   end
   
   def articles_list(args)
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     @articles = Article.published.not_reserved.all
-    @snip_id = args[:snip_id]
     render
   end
   
   def article_show(args)
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     @article = args[:article]
-    @snip_id = args[:snip_id]
     render
   end
   
@@ -38,5 +37,18 @@ class ContentCell < Cell::Rails
   #   render
   # end
   
+  private
+  
+  def fill_generic_variables(page, options, snip_id)
+    @page = page ||= yard_home
+    @options = options ||= {}
+    @snip_id = snip_id
+    @cfg = cfg
+  end
+
+  def cfg
+    Setting.first
+  end
+  memoize :cfg
   
 end
--- app/cells/menu_cell.rb
+++ app/cells/menu_cell.rb
@@ -1,75 +1,64 @@
 class MenuCell < Cell::Rails
+  
+  extend ActiveSupport::Memoizable
   include Yard
   helper_method :yard_home, :get_lang, :get_yard_url, :get_article_url # coming from lib/yard
   
   def first_level(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
-    @first_level_pages = yard_home.siblings
-    @snip_id = args[:snip_id]
-    @options = args[:options] ||= {}
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
+    @first_level_pages = yard_home.siblings 
     render
   end
 
   def two_levels(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
-    @first_level_pages = yard_home.siblings
-    @snip_id = args[:snip_id]
-    @options = args[:options] ||= {}
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
+    @first_level_pages = yard_home.siblings
     render
   end
 
   def siblings(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
-    @snip_id = args[:snip_id]
-    @options = args[:options] ||= {}
     render
   end
 
   def children(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
-    @options = args[:options] ||= {}
-    @snip_id = args[:snip_id]
     render
   end
 
   def siblings_and_children(args)
-    @page = args[:page]
-    @options = args[:options] ||= {}
-    @page = yard_home if @page.blank?
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
-    @snip_id = args[:snip_id]
     render
   end
 
   def footer(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
-    @first_level_pages = yard_home.siblings
-    @options = args[:options] ||= {}
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
     fill_menu_variables(@page)
-    @snip_id = args[:snip_id]
+    @first_level_pages = yard_home.siblings 
     render
   end
   
   def nav(args)
-    @page = args[:page]
-    @page = yard_home if @page.blank?
+    fill_generic_variables(args[:page], args[:options], args[:snip_id])
+    fill_menu_variables(@page)
     @ancestors = @page.ancestors
-    @options = args[:options] ||= {}
     @ancestors << @page
-    @snip_id = args[:snip_id]
     render
   end
   
   private
+  
+  def fill_generic_variables(page, options, snip_id)
+    @page = page ||= yard_home
+    @options = options ||= {}
+    @snip_id = snip_id
+    @cfg = cfg
+  end
   
   def fill_menu_variables(page)
     @current_level_pages = @page.siblings
@@ -77,6 +66,11 @@ class MenuCell < Cell::Rails
     @family_pages_ids << @page.ancestors.map{|a| a.id}
     @family_pages_ids = @family_pages_ids.compact.flatten   
   end
+
+  def cfg
+    Setting.first
+  end
+  memoize :cfg
 
 
 end
