d20d0dfbba39289e25cb421b9d10683bde9430cb
Silvio Relli <silvio@relli.org>
2011-10-13 17:29:31 +0200

    Articles language filter in backend

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -9,9 +9,10 @@ class Admin::ArticlesController < Admin::AdminController
   authorize_resource
   
   def index
-    @categories = Category.all
-    @articles = Article.all
     @admin_editing_language = admin_editing_language
+    @categories = Category.all
+    @articles = Article.where("lang = ?", @admin_editing_language)
+    
   end
   
   def new
@@ -21,12 +22,9 @@ class Admin::ArticlesController < Admin::AdminController
   end
   
   def create
-    @categories = Category.all
     @article = current_user.articles.new(params[:article])
     @article.pretty_url = @article.pretty_url.urlify.blank? ? @article.title.urlify : @article.pretty_url.urlify
     @article.publish_at = Time.now if @article.published
-    
-    
     if @article && @article.save && @article.errors.empty?
       redirect_to admin_articles_path
     else
@@ -35,6 +33,7 @@ class Admin::ArticlesController < Admin::AdminController
   end
   
   def edit
+    @categories = Category.all
     @admin_editing_language = admin_editing_language
     @article = Article.find(params[:id])
   end
