6eab542e1a8acb3d89a661efa0adebba8eae2f02
Silvio Relli <silvio@relli.org>
2011-10-27 16:50:27 +0200

    Fixed little error in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :articles, :through => :categorizations
-  has_many :categories, :through => :categorizations
   
   validates_presence_of :name
   validates_length_of :name, :minimum => 3
