1da198580905fc760d702caf96b719772e9512b2
Silvio Relli <silvio@relli.org>
2011-11-23 14:32:49 +0100

    Better themes and layouts error handling

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -1,8 +1,11 @@
 class Layout
+  extend ActiveSupport::Memoizable
   attr_reader :title, :filename, :path, :areas
   
   ## Exception Handling
-  class ThemeNotFound < StandardError
+  class LayoutNotFound < StandardError
+  end
+  class LayoutsLocatorError < StandardError
   end
   
   # title = descriptive title
@@ -17,22 +20,29 @@ class Layout
     extend ActiveSupport::Memoizable
     
     def all(theme_name)
-      theme = Theme.find(theme_name)
-      theme_conf = YAML.load_file("#{theme.path}/theme_conf.yml")
-      layouts = []
-      theme_conf["layouts"].map do |lay|
-        layouts << self.new(lay["title"], lay["filename"], "#{theme.path}/views/layouts/#{lay["filename"]}", lay["areas"])
+      begin
+        theme = Theme.find(theme_name)
+        theme_conf = YAML.load_file("#{theme.path}/theme_conf.yml")
+        layouts = []
+        theme_conf["layouts"].map do |lay|
+          layouts << self.new(lay["title"], lay["filename"], "#{theme.path}/views/layouts/#{lay["filename"]}", lay["areas"])
+        end
+        layouts.compact.flatten.uniq
+      rescue Exception => exc
+         Rails.logger.error("***************** Error with layouts locator *****************")
+         raise LayoutsLocatorError
       end
-      layouts.compact.flatten.uniq
     end
     memoize :all
     
     def find(theme, filename) 
       begin
-        all(theme.to_s).select{|t| t.filename.eql? filename.to_s}.first
+        lay = all(theme.to_s).select{|t| t.filename.eql? filename.to_s}.first
+        raise LayoutNotFound if lay.nil?
+        lay
       rescue Exception => exc
-         Rails.logger.error("***************** Error locating theme *****************")
-         raise ThemeNotFound
+         Rails.logger.error("***************** Error locating layout *****************")
+         raise LayoutNotFound
       end
     end
     memoize :find
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,12 @@
 class Theme
   extend ActiveSupport::Memoizable
-  
   attr_reader :path, :short, :title, :description, :author
+  
+  ## Exception Handling
+  class ThemeNotFound < StandardError
+  end
+  class ThemesLocatorError < StandardError
+  end
   
   # path = absolute theme directory path on filesystem
   # short = theme directory name, must be the same as the base_conf.short entry on the theme_conf.yaml file
@@ -12,27 +17,45 @@ class Theme
     @path, @short, @title, @description, @author = path, short, title, description, author
   end
   
-  def self.all
-    themes = []
-    search_themes.map do |theme_path|
-      theme_conf = YAML.load_file("#{theme_path}/theme_conf.yml")
-      themes << self.new(theme_path, theme_conf["base_conf"]["short"], theme_conf["base_conf"]["title"], theme_conf["base_conf"]["description"], theme_conf["base_conf"]["author"])
+  # Thats the only way to memoize class methods (self methods)
+  class << self
+    extend ActiveSupport::Memoizable
+  
+    def all
+      begin
+        themes = []
+        search_themes.map do |theme_path|
+          theme_conf = YAML.load_file("#{theme_path}/theme_conf.yml")
+          themes << self.new(theme_path, theme_conf["base_conf"]["short"], theme_conf["base_conf"]["title"], theme_conf["base_conf"]["description"], theme_conf["base_conf"]["author"])
+        end
+        themes.compact.uniq
+      rescue Exception => exc
+         Rails.logger.error("***************** Error with themes locator *****************")
+         raise ThemesLocatorError
+      end
     end
-    themes.compact.uniq
-  end
-  #memoize :all
+    memoize :all
+    
+    def find(short)
+      begin
+        theme = all.select{|t| t.short.eql? short.to_s}.first
+        raise ThemeNotFound if theme.nil?
+        theme
+      rescue Exception => exc
+         Rails.logger.error("***************** Error locating theme *****************")
+         raise ThemeNotFound
+      end
+    end
+    memoize :find
+    
+    def search_themes
+      themes_path = "#{Rails.root.to_s}/themes/[a-zA-Z0-9]*"
+      Dir.glob(themes_path).select do |file|
+        File.readable?("#{file}/theme_conf.yml")
+      end.compact.uniq
+    end
+    memoize :search_themes
   
-  def self.find(short)
-    all.select{|t| t.short.eql? short.to_s}.first
   end
-  #memoize :find
-  
-  def self.search_themes
-    themes_path = "#{Rails.root.to_s}/themes/[a-zA-Z0-9]*"
-    Dir.glob(themes_path).select do |file|
-      File.readable?("#{file}/theme_conf.yml")
-    end.compact.uniq
-  end
-  #memoize :search_themes
   
 end
