3ef7de52f306ab9270973b546edae3f340e89af9
Silvio Relli <silvio@relli.it>
2011-12-13 15:11:15 +0100

    Fixed Cucumber deprecation warnings

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,6 +1,6 @@
 Given /^I am logged in as "([^"]*)" with password "([^"]*)"$/ do |email, password|
-  Given "I go to the admin page"
-  Given "I fill in \"user_email\" with \"#{email}\""
-  Given "I fill in \"user_password\" with \"#{password}\""
-  Given "I press \"Sign in\""
+  step "I go to the admin page"
+  step "I fill in \"user_email\" with \"#{email}\""
+  step "I fill in \"user_password\" with \"#{password}\""
+  step "I press \"Sign in\""
 end
