406471978ed84c74ebfc75c8b4f4a782c887c11b
Silvio Relli <silvio@relli.it>
2012-01-02 15:36:14 +0100

    Signup mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,20 @@ Railsyard2::Application.configure do
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'gmail.com',
+  #   :user_name            => 'username@gmail.com',
+  #   :password             => 'password',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true
+  # }
+  
+  
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,6 @@ Railsyard2::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
-
   # Enable threaded mode
   # config.threadsafe!
 
@@ -50,5 +47,20 @@ Railsyard2::Application.configure do
   
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'gmail.com',
+  #   :user_name            => 'username@gmail.com',
+  #   :password             => 'password',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true
+  # }
+  
   
 end
