997da70afa0e5823b9be31931d3705a3c125fc33
Silvio Relli <silvio@relli.it>
2012-01-10 11:10:55 +0100

    Fixed language change in articles bug

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,11 +2,13 @@ class Admin::PagesController < Admin::AdminController
   
   ## manual authorization
   before_filter do |controller|
-    controller.check_role("admin")
+    controller.check_role("admin") unless (controller.action_name == "set_editing_language")
   end
   
   ## CanCan authorization - see Ability model
   authorize_resource
+  #skip_authorize_resource :only => :set_editing_language
+  skip_authorization_check :only => :set_editing_language
   
   def index
     @admin_editing_language = admin_editing_language
