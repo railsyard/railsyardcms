83c92e21046f715655d1b8a3b58fcb8c546c202c
nebirhos <nebirhos@aol.com>
2012-05-07 16:46:10 +0200

    Articles should be in reverse published order

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,7 +18,8 @@ class Article < ActiveRecord::Base
                     :styles => {:large => "500x500>", :medium => "300x300>", :thumb => "100x100>", :highlight => "307x132>" },
                     :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension",
                     :url => "/system/:class/:attachment/:id/:style/:basename.:extension"
-  
+
+  default_scope :order => 'publish_at DESC'
   scope :drafts, :conditions => ["published = ?", false]
   scope :not_reserved, :conditions => ["reserved = ?", false]
   scope :for_public_feed, :conditions => ["reserved = ? AND published = ?", false, true], :order => "publish_at DESC" 
